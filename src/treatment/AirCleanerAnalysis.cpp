c0dd73627d013f3b8f81112fa2eb4e66b03b2851
clemgi0 <clement.giraudon2@gmail.com>
2023-05-30 16:52:21 +0200

    again
    efficiency check

--- src/treatment/AirCleanerAnalysis.cpp
+++ src/treatment/AirCleanerAnalysis.cpp
@@ -35,20 +35,41 @@ AirCleanerAnalysis::~AirCleanerAnalysis() {
   #endif
 }
 
-int AirCleanerAnalysis::checkEfficiency(const AirCleaner & airCleaner, const vector<Sensor*> & sensors) const {
-  int somme(0);
+int AirCleanerAnalysis::checkEfficiency(AirCleaner & airCleaner, const vector<Sensor*> & sensors) const {
+  double somme(0);
+  double nb = 0;
 
-<<<<<<< Updated upstream
-  //for(int )
-=======
   bool efficace = true;
 
-  int distance(100);
+  double distance(100);
 
   while(efficace){
-    
-  }
->>>>>>> Stashed changes
+    cout << "efficace : " << efficace << " / distance : " << distance << " / somme : " << somme << " / nb : " << nb << endl;
+    vector<Sensor*>::const_iterator it;
+    double bestDistance = __INT_MAX__;
+    Sensor * s;
 
-  return somme;
+    for (it = sensors.begin() ; it != sensors.end(); ++it){
+      s = *it;
+      //cout << "Pour le capteur " << s->getSensorID() << " situé en " << s->getPosition() << endl;
+
+      multimap<double,Sensor*> distanceMapSensors;
+      double currentDistance = airCleaner.getPosition().calculateDistance(s->getPosition());
+      if(currentDistance < distance && currentDistance > bestDistance)
+        bestDistance = currentDistance;      
+    }
+
+    Date avant("2019-01-31 12:00:00");
+    Date apres("2019-03-02 00:00:00");
+    cout << "best distance : " << bestDistance << " / pour le capteur : " << s->getSensorID() << " / valeur en N02 : " << s->getValNO2().find(avant)->second << " / valeur en N02 après : " << s->getValNO2().find(apres)->second*1.15 << endl;
+    if(s->getValNO2().find(avant)->second > s->getValNO2().find(apres)->second*1.15 && s->getValO3().find(avant)->second > s->getValO3().find(apres)->second*1.15 && s->getValPM10().find(avant)->second > s->getValPM10().find(apres)->second*1.15 && s->getValSO2().find(avant)->second > s->getValSO2().find(apres)->second*1.15)
+      distance += 100;
+    else
+      efficace = false;
+
+    somme += distance;
+    nb += 1;
+  }
+
+  return somme/nb;
 }
